69e0b1fb1c8fde5985b272d97f94ebaa66ebe11a
Volodymyr <vovastalker26@gmail.com>
2020-05-08 00:50:15 +0300

    fix

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const MenuItems = () => {
   return (
     <div className='mt-4 '>
-      <h4>Ви нас можете знайти тут:</h4>
+      <h4>You can find us here:</h4>
       <a href="https://www.facebook.com/">
         Facebook
       </a>
-      <p><strong>+380973311444</strong>{' '}цілодобово, без вихідних: 24/7</p>
+      <p><strong>+380973311444</strong>{' '}we work: 24/7</p>
     </div>
   );
 }
