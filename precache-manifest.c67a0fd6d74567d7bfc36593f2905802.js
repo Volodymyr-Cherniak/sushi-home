472df5c729ce9e3a8077513da5bb35cafc3953b3
Volodymyr <vovastalker26@gmail.com>
2020-05-16 20:59:53 +0300

    Updates

---
+++ precache-manifest.c67a0fd6d74567d7bfc36593f2905802.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05704c8d974c0f5f8eb84fc39902535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afcfce8ec669beb333a0",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "cce388b529fe39a9b4c9",
+    "url": "/static/css/main.a47a8f40.chunk.css"
+  },
+  {
+    "revision": "afcfce8ec669beb333a0",
+    "url": "/static/js/2.18cba099.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.18cba099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce388b529fe39a9b4c9",
+    "url": "/static/js/main.3a0b99cf.chunk.js"
+  },
+  {
+    "revision": "d38776c54777e9547793",
+    "url": "/static/js/runtime-main.35c6da4a.js"
+  },
+  {
+    "revision": "15c1d70facad77185d3e6c57bf456e20",
+    "url": "/static/media/20190919_114832_гонконг.15c1d70f.jpg"
+  },
+  {
+    "revision": "46d71a3e1447d081ddf338c91d58f4d2",
+    "url": "/static/media/20190919_115057_масаго з лосоем.46d71a3e.jpg"
+  },
+  {
+    "revision": "2780da0891fa6bad8d1cf48c7feccb56",
+    "url": "/static/media/20190919_115312_рол з копченим лососем.2780da08.jpg"
+  },
+  {
+    "revision": "8ba9f04ddd3ebbfbd75a14584df82f12",
+    "url": "/static/media/20190919_115708_філ. з авокадо.8ba9f04d.jpg"
+  },
+  {
+    "revision": "f8ac069325407ed98fbdb01c78f56dba",
+    "url": "/static/media/20190919_115733_філад. з масялною рибою.f8ac0693.jpg"
+  },
+  {
+    "revision": "9e2d2ff208787e1c2eb11e25983993cb",
+    "url": "/static/media/20200312_161001_1111.9e2d2ff2.jpg"
+  }
+]);
