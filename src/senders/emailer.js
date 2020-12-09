b9967c57053eb10eaed8b05807238cc3bd1e04ed
Volodymyr <vovastalker26@gmail.com>
2020-12-09 16:53:51 +0200

    upd http methods upd package.json

--- src/senders/emailer.js
+++ src/senders/emailer.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import {address} from "../constans";
+import apiUrl from "../utils/httpMethods";
 
 export default function sendEmail(args, itemsInCart, setSuccess, clearCart) {
   const cartItems = itemsInCart.map(el => ({name: el.name, count: el.count}));
   axios
-    .post(`${address.remote}/email`, {...args, cartItems})
+    .post(`${apiUrl}/email`, {...args, cartItems})
     .then(res => {
       console.log(res)
       if (res) {
